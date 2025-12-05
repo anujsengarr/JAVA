ce7b1409f4fcbcc2a3a8083a9aa7c8e353e68298
Anuj Sengar <anujsengar2456@gmail.com>
2025-12-05 14:29:25 +0530

    commit

---
+++ recursion_fibonacci.java
@@ -0,0 +1,17 @@
+public class recursionFibonacci {
+    public static void main(String[] args) {
+        int n = 10; // Example: Find the 10th Fibonacci number
+        int result = fibonacci(n);
+        System.out.println("The " + n + "th Fibonacci number is: " + result);
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
