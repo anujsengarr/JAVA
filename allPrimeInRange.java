59905efbebbb3261da7f8255d2871e1cbeb1789c
Anuj Sengar <anujsengar2456@gmail.com>
2025-12-05 15:29:14 +0530

    DONE

---
+++ allPrimeInRange.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class allPrimeInRange {
+    public static boolean InRangeofPrime(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter starting number: ");
+        int a=sc.nextInt();
+        System.out.println("Enter ebding number:");
+        int b=sc.nextInt();
+        System.out.println("Prime number between" +a+ "and" +b+ "are:" );
+        for(int i=a;i<=b;i++){
+            boolean isPrime=true;
+
+            if(i<=1);
+              isPrime=false;
+            else{
+                for(int j)
+            }
+
+        }
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
