39e9de79c08fee63d61db6f0cc7dd31aa990c67a
Anuj Sengar <anujsengar2456@gmail.com>
2025-12-05 15:14:30 +0530

    COMMIT

--- forLoop.java
+++ forLoop.java
@@ -1,3 +1,8 @@
 public class forLoop {
-    
+      public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println("Aditya Singh");
+        }
+        
+    }
 }
